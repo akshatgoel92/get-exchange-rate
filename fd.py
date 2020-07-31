19075b539c4384fc8f835edb5bf9bdb6eaac986a
akshatgoel92 <akshatgoel92@hotmail.com>
2020-08-01 03:41:36 +0530

    Add updated FD script

--- fd.py
+++ fd.py
@@ -1,16 +1,35 @@
 # Import packages
 import argparse
+import numpy as np
 
 
-def get_fd_value(principal, rate, period):
+def get_one_time_fd_value(principal, rate, period):
     '''
     ---------------
     Execute the function calls.
     ---------------
     '''
-    return((principal*(1 - rate**period))/(1-rate))
+    return(principal*(rate**period))
 
 
+def get_recurring_fd_value(principal, rate, period):
+    '''
+    ---------------
+    Execute the function calls.
+    ---------------
+    '''
+    amounts = [get_one_time_fd_value(principal, rate, tenure) for tenure in range(period + 1)]
+    return(np.sum(np.array(amounts)))    
+
+def get_amount(amount, rate, period):
+    '''
+    ---------------
+    Execute the function calls.
+    ---------------
+    '''
+    principal = amount/(rate**period)
+    return(principal)
+    
 def parse_args():
     '''
     ---------------
@@ -18,30 +37,36 @@ def parse_args():
     ---------------
     '''
     parser = argparse.ArgumentParser(description='Process inputs to exchange rate routine.')
-    
     parser.add_argument('--rate', type = float, help = 'Assumed FD rate of interest.', required = False)
     parser.add_argument('--period', type = int, help = 'No. of periods FD will be held for.', required = False)
     parser.add_argument('--principal', type = int, help = 'The total initial investment in rupees.', required = False)
+    parser.add_argument('--r', dest='accumulate', action='store_const', const=get_recurring_fd_value, 
+                        default=get_one_time_fd_value, help='sum the integers (default: find the max)')
+    parser.add_argument('--a', dest='accumulate', action='store_const', const=get_amount, 
+                        default=get_one_time_fd_value, help='sum the integers (default: find the max)')
     
     args = parser.parse_args()
     
     rate = args.rate
     period = args.period
     principal = args.principal
+    recurring = args.accumulate
     
-    return(rate, period, principal)
+    print(args)
+    
+    return(rate, period, principal, recurring)
 
-    
+
 def main():
     '''
     ---------------
     Execute the function calls.
     ---------------
     '''
-    # Get the arguments
-    rate, period, principal = parse_args()
-    amount = get_fd_value(principal, rate, period)
-    print('The total final value of this FD investment is {}'.format(amount))
+    msg = 'The total final value of this FD investment is {}'
+    rate, period, principal, recurring = parse_args()
+    amount = recurring(principal, rate, period)
+    print(msg.format(amount))
 
     return
 
