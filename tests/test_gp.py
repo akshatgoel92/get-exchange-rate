18b7c24692086e923a6ebff467ecd03b01a02c4d
akshatgoel92 <akshatgoel92@hotmail.com>
2020-08-30 12:50:53 +0530

    Add basic type checking tests for exact GP model

--- tests/test_gp.py
+++ tests/test_gp.py
@@ -1,17 +1,24 @@
-import unittest
-import model.gp_test as gp
-
-import plotly.graph_objects as go
+import models.gp_test as gp
 import pandas as pd
 import numpy as np
+import unittest
+import gpytorch
+import torch
+import math
 
+from gpytorch.likelihoods.gaussian_likelihood import GaussianLikelihood
 
 
 class TestExactGP(unittest.TestCase):
 
 
     def setUp(self):
-        
+        '''
+        ----------------------
+        > Input: 
+        > Output: 
+        ----------------------
+        '''
         # Set seed
         torch.manual_seed(10391)
         # Set training iterations
@@ -21,24 +28,30 @@ class TestExactGP(unittest.TestCase):
         # Training data is 100 points in [0,1] inclusive regularly spaced
         self.train_x = torch.linspace(0, 1, 100)
         # True function is sin(2*pi*x) with Gaussian noise
-        self.train_y = torch.sin(train_x * (2 * math.pi)) + torch.randn(train_x.size()) * math.sqrt(0.04)
+        self.train_y = torch.sin(self.train_x * (2 * math.pi)) + torch.randn(self.train_x.size()) * math.sqrt(0.04)
         # Train model and get likelihood
-        self.model, self.likelihood = gp.train()
+        self.model, self.likelihood = gp.train(self.train_x, self.train_y, self.training_iter, self.lr)
 
 
     def test_train_model_type(self):
-        self.assertEqual(type(gp.train(**self.train_args)), 200)
+        '''
+        ----------------------
+        > Input: 
+        > Output: 
+        ----------------------
+        '''
+        self.assertEqual(type(self.model), gp.ExactGPModel)
 
-    def test_train_likelihood_type(self):
-        self.assertEqual(type(gp.train(**self.train_args), 200)
-
-    def test_train_df_shape(self)
-        self.assertEqual(gp.train(**self.train_args), 200)
     
-    def tearDown(self):
+    def test_train_likelihood_type(self):
+        '''
+        ----------------------
+        > Input: 
+        > Output: 
+        ----------------------
+        '''
+        self.assertEqual(type(self.likelihood), GaussianLikelihood)
 
-        del self.train_x
-        del self.train_y
 
 
 if __name__ == '__main__':
