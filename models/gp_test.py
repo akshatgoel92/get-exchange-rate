a64bc4f00c522a062431c346428d68528243dcc1
akshatgoel92 <akshatgoel92@hotmail.com>
2020-08-30 08:32:42 +0530

    Add next pass at GP regression

--- models/gp_test.py
+++ models/gp_test.py
@@ -40,52 +40,51 @@ def get_data(df):
     Convert data to tensors
     ________________________
     '''
-    # Training data is 100 points in [0,1] inclusive regularly spaced
+    # Prepare the training data
     train_x = torch.from_numpy(np.array(df['INR'].shift(1)[1:])).float()
     train_y = torch.from_numpy(np.array(df['INR'][:-1])).float()
 
+    # Wrap the training data in a variable object
     train_x = torch.autograd.Variable(train_x).float()
     train_y = torch.autograd.Variable(train_y).float()
 
     return(train_x, train_y)
 
 
-def train(train_x, train_y, training_iter):
+def train(train_x, train_y, training_iter, lr):
     '''
     ___________________
     Training loop
     ___________________
     '''
-    # Initialize likelihood and model
+    # Initialize likelihood
     likelihood = gpytorch.likelihoods.GaussianLikelihood()
+    # Initialize model
     model = ExactGPModel(train_x, train_y, likelihood)
-
     # Find optimal model hyperparameters
     model.train()
+    # Find optimal likelihood parameters
     likelihood.train()
-
-    # Use the adam optimizer
-    optimizer = torch.optim.Adam([{'params': model.parameters()}, ], lr=0.1)
-
-    # "Loss" for GPs - the marginal log likelihood
+    # Use the Adam optimizer
+    optimizer = torch.optim.Adam([{'params': model.parameters()}, ], lr=lr)
+    # "Loss" for GPs: the marginal log likelihood
     mll = gpytorch.mlls.ExactMarginalLogLikelihood(likelihood, model)
-
+    # Enter the training loop
     for i in range(training_iter):
-        
         # Zero gradients from previous iteration
         optimizer.zero_grad()
         # Output from model
         output = model(train_x)
-        
-        # Calculate loss and backprop gradients
+        # Calculate loss
         loss = -mll(output, train_y)
+        # Calculate graduents on the loss function
         loss.backward()
-        
+        # Print progress of the model
         print('Iter %d/%d - Loss: %.3f   lengthscale: %.3f   noise: %.3f' % (
             i + 1, training_iter, loss.item(),
             model.covar_module.base_kernel.lengthscale.item(),
             model.likelihood.noise.item()))
-        
+        # One calculation step
         optimizer.step()
 
     return(model, likelihood)
@@ -97,21 +96,27 @@ def predict(model, likelihood, spot_rate, size=1000):
     Get posterior + predictive
     ___________________________
     '''
+    # Create a tensor from the spot rate scalar
     test_x = torch.tensor([spot_rate]).float()
+    # Wrap the test data in a Variable object
     test_x = torch.autograd.Variable(test_x).float()
-
+    # Put model into evaluation mode
     model.eval()
+    # Put likelihood into evaluation mode
     likelihood.eval()
-
+    # Get posterior distribution
     f_preds = model(test_x)
+    # Get predictive distribution
     y_preds = likelihood(model(f_preds))
-
+    # Get posterior mean
     f_mean = f_preds.mean
+    # Get posterior variance
     f_var = f_preds.variance
-    
+    # Get posterior covariance
     f_covar = f_preds.covariance_matrix
+    # Get samples from the posterior
     f_samples = f_preds.sample(sample_shape=torch.Size((size,)))
-
+    # Return statement
     return(f_preds, y_preds, f_mean, f_var, f_covar, f_samples)
 
 
@@ -121,8 +126,11 @@ def main(df, spot_rate):
     Execute code
     ___________________
     '''
+    # Set seed for reproducible results
     torch.manual_seed(10)
+    # Get the training data
     train_x, train_y = get_data(df)
-    
+    # Get the model and likelihood objects from training
     model, likelihood = train(train_x, train_y)
+    # Get the results we need
     f_preds, y_preds, f_mean, f_var, f_covar, f_samples = predict(model, likelihood, spot_rate)
